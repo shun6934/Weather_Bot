39d2e1d0fc2e48eecffeda2bc7359fc65a741553
shun6934 <b1612851@planet.kanazawa-it.ac.jp>
2018-10-23 16:32:25 +0900

    YOLP APIの取得

--- main.go
+++ main.go
@@ -1,2 +1,35 @@
 package main
 
+import(
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+)
+
+func main() {
+    values := url.Values{}
+    base_url := "https://map.yahooapis.jp/weather/V1/place?"
+
+    values.Add("coordinates", "36.530213,136.627226")
+    values.Add("appid", "dj00aiZpPUd0aTBqdms3dW1sOCZzPWNvbnN1bWVyc2VjcmV0Jng9YzU-")
+    values.Add("output", "json")
+    resp, err := http.Get(base_url + values.Encode())
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    defer resp.Body.Close()
+
+    execute(resp)
+}
+
+func execute(response *http.Response) {
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Println(string(body))
+}
